7d60be1e50fe82e7ee82d3f10d41661c67a6c551
Michael Lee <jjjamdmin@outlook.com>
2019-01-09 20:56:31 +0800

    修复用户点击网页上面的取消时崩溃的问题

--- src/main/java/com/microsoft/services/msa/AuthorizationRequest.java
+++ src/main/java/com/microsoft/services/msa/AuthorizationRequest.java
@@ -495,7 +495,7 @@ class AuthorizationRequest implements ObservableOAuthRequest, OAuthRequestObserv
 
         if (hasQueryParameters && !isHierarchical) {
             String[] pairs = endUri.getQuery().split("&|=");
-            for (int i = 0; i < pairs.length; i =+ 2) {
+            for (int i = 0; i < pairs.length; i += 2) {
                 if (pairs[i].equals(OAuth.CODE)) {
                     this.onAuthorizationResponse(pairs[i + 1]);
                     return;
